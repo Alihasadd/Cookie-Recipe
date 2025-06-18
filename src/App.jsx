ca69273968a3abe42508e324395f5a5b0b790060
Ali Vidler <alisonvidler@gmail.com>
2025-06-17 23:56:52 -0400

    Adding props in

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { useState } from "react"
 
 
 const App = () => {
-  const [ isModalOpen, setIsModalOpen ] = useState(true)
+  const [ isModalOpen, setIsModalOpen ] = useState(false)
   return <>
     <div className="bg-purple-500 w-full justify-center">
       <div className="justify-between font-bold text-white flex p-4 border-4 border-amber-300">
         <div>Santi's Cookie Recipe</div>
         <div className="flex items-center gap-2">
-        <button>Sign In</button>
+        <button Onclick={setIsModalOpen}>Sign In</button>
         <i class="fa-solid fa-right-to-bracket"></i>
         </div>
         
@@ -18,7 +18,7 @@ const App = () => {
       
     </div>
     <CookieRecipe />
-    {isModalOpen && <Modal /> }
+    {isModalOpen && <Modal /> Onclose={() => setIsModalOpen(false)}}
   </>;
 };
 
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Modal = () => {
+const Modal = (props) => {
+    const { onClose } = props;
     return (
         <div className="bg-stone-700/80 justify-center items-center fixed h-full w-full top-0 left-0 flex gap-4 backdrop-blur-sm">
             <div className="bg-white h-80 w-80 p-4 rounded shadow-lg justify-center items-center flex flex-col">
                  This is a modal 
-                <button>Click here to close</button>
+                <button >Click here to close</button>
             </div>
         </div>
     )
