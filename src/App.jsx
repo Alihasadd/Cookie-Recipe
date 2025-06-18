6b6b2674599a3506a72cc1688db59e61cff43065
Ali Vidler <alisonvidler@gmail.com>
2025-06-17 23:50:33 -0400

    Use state working

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Modal from "./Modal";
+import CookieRecipe from "./CookieRecipe";
+import { useState } from "react"
+
+
+const App = () => {
+  const [ isModalOpen, setIsModalOpen ] = useState(true)
+  return <>
+    <div className="bg-purple-500 w-full justify-center">
+      <div className="justify-between font-bold text-white flex p-4 border-4 border-amber-300">
+        <div>Santi's Cookie Recipe</div>
+        <div className="flex items-center gap-2">
+        <button>Sign In</button>
+        <i class="fa-solid fa-right-to-bracket"></i>
+        </div>
+        
+      </div>
+      
+    </div>
+    <CookieRecipe />
+    {isModalOpen && <Modal /> }
+  </>;
+};
+
+export default App;
---
+++ src/Modal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Modal = () => {
+    return (
+        <div className="bg-stone-700/80 justify-center items-center fixed h-full w-full top-0 left-0 flex gap-4 backdrop-blur-sm">
+            <div className="bg-white h-80 w-80 p-4 rounded shadow-lg justify-center items-center flex flex-col">
+                 This is a modal 
+                <button>Click here to close</button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
