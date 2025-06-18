5bd74ae3cad731a8453944079b2208709df72953
Ali Vidler <alisonvidler@gmail.com>
2025-06-18 00:01:31 -0400

    It worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import Modal from "./Modal";
 import CookieRecipe from "./CookieRecipe";
-import { useState } from "react"
-
+import { useState } from "react";
 
 const App = () => {
-  const [ isModalOpen, setIsModalOpen ] = useState(false)
-  return <>
-    <div className="bg-purple-500 w-full justify-center">
-      <div className="justify-between font-bold text-white flex p-4 border-4 border-amber-300">
-        <div>Santi's Cookie Recipe</div>
-        <div className="flex items-center gap-2">
-        <button Onclick={setIsModalOpen}>Sign In</button>
-        <i class="fa-solid fa-right-to-bracket"></i>
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <div className="bg-purple-500 w-full justify-center">
+        <div className="justify-between font-bold text-white flex p-4 border-4 border-amber-300">
+          <div>Santi's Cookie Recipe</div>
+          <div className="flex items-center gap-2">
+            <button onClick={setIsModalOpen}>Sign In</button>
+            <i class="fa-solid fa-right-to-bracket"></i>
+          </div>
         </div>
-        
       </div>
-      
-    </div>
-    <CookieRecipe />
-    {isModalOpen && <Modal /> Onclose={() => setIsModalOpen(false)}}
-  </>;
+      <CookieRecipe />
+      {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
+    </>
+  );
 };
 
 export default App;
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = (props) => {
         <div className="bg-stone-700/80 justify-center items-center fixed h-full w-full top-0 left-0 flex gap-4 backdrop-blur-sm">
             <div className="bg-white h-80 w-80 p-4 rounded shadow-lg justify-center items-center flex flex-col">
                  This is a modal 
-                <button >Click here to close</button>
+                <button onClick={onClose}>Click here to close</button>
             </div>
         </div>
     )
